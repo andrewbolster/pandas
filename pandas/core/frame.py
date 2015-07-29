a3bef8268b542ad935e324e34826993ba7d6b430
Andrew Bolster <me@andrewbolster.info>
2015-07-29 12:42:33 +0100

    ENH: Bug in ``column_format`` propagation to ``DataFrameFormatter`` in ``DataFrame.to_latex`` (#9402)

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -1404,7 +1404,8 @@ class DataFrame(NDFrame):
     def to_latex(self, buf=None, columns=None, col_space=None, colSpace=None,
                  header=True, index=True, na_rep='NaN', formatters=None,
                  float_format=None, sparsify=None, index_names=True,
-                 bold_rows=True, longtable=False, escape=True):
+                 bold_rows=True, longtable=False, escape=True,
+                 column_format=None):
         """
         Render a DataFrame to a tabular environment table. You can splice
         this into a LaTeX document. Requires \\usepackage{booktabs}.
@@ -1416,6 +1417,9 @@ class DataFrame(NDFrame):
         longtable : boolean, default False
             Use a longtable environment instead of tabular. Requires adding
             a \\usepackage{longtable} to your LaTeX preamble.
+        column_format : str, default None
+            Provide a custom column alignment as part of the table environment
+            setup.
         escape : boolean, default True
             When set to False prevents from escaping latex special
             characters in column names.
@@ -1436,7 +1440,7 @@ class DataFrame(NDFrame):
                                            sparsify=sparsify,
                                            index_names=index_names,
                                            escape=escape)
-        formatter.to_latex(longtable=longtable)
+        formatter.to_latex(longtable=longtable, column_format=column_format)
 
         if buf is None:
             return formatter.buf.getvalue()
